4cd3bc95b087995ab217754a677518be484709c2
bapboy18 <bapboy18@yahoo.com.vn>
2015-06-08 16:56:14 +0700

    Used I18n

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 50.times do
   FactoryGirl.create :user
 end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
