ebc0bc9e7989a65d3e67b468d992b0271950734e
HoangVanTrinh92 <hoang.van.trinh@framgia.com>
2015-06-11 10:23:27 +0700

    CRUD-team

---
+++ app/controllers/admin/teams_controller.rb
@@ -0,0 +1,56 @@
+class Admin::TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @teams = Team.all.paginate page: params[:page], per_page: Settings.paginate.normal
+  end
+
+  def show
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def edit
+  end
+
+  def create
+    @team = Team.new team_params
+
+    respond_to do |format|
+      if @team.save
+        format.html {redirect_to [:admin, @team], notice: t("team.create")}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @team.update team_params
+        format.html {redirect_to [:admin, @team], notice: t("team.update")}
+      else
+        format.html {render :edit}
+      end
+    end
+  end
+
+  def destroy
+    @team.destroy
+    respond_to do |format|
+      format.html {redirect_to admin_teams_url, notice: t("team.delete")}
+      format.json {head :no_content}
+    end
+  end
+
+  private
+  def set_team
+    @team = Team.find params[:id]
+  end
+
+  def team_params
+    params.require(:team).permit :name, :description, :leader
+  end
+end
---
+++ spec/factories/teams.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :team do
+    name "Team1"
+    description "Lam Project1"
+    leader "Hoang Trinh"
+  end
+end
