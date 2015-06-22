dd60de2a387983580d857ab5b8b75c7817993da8
HoangVanTrinh92 <hoang.van.trinh@framgia.com>
2015-06-22 16:27:04 +0700

    Refactor-code-1

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -21,10 +21,10 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t("flash.added")
+      flash[:success] = t "flash.added"
       redirect_to admin_users_url
     else
-      render 'new'
+      render "new"
     end
   end
 
@@ -35,17 +35,17 @@ class Admin::UsersController < ApplicationController
   def update
     @user = User.find params[:id]
     if @user.update_attributes user_params
-      flash[:success] = t("flash.update")
+      flash[:success] = t "flash.update"
       redirect_to admin_users_url
     else
-      render 'edit'
+      render "edit"
     end
   end
 
   def destroy
     @user = User.find params[:id]
     @user.destroy
-    flash[:notice] = t("flash.delete")
+    flash[:notice] = t "flash.delete"
     redirect_to admin_users_path
   end
 
